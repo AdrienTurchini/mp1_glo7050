576dfc2461cc85c8e9f484d21444e1270e0746b7
peopleOfPalay <marck.henriot@gmail.com>
2021-02-11 17:26:46 +0100

    poly_20

--- main.py
+++ main.py
@@ -18,3 +18,9 @@ with open('Datasets\Dataset_1_valid.csv') as csvfile:
     data = data[:, :2].astype(np.float)
     X1_valid = data[:, 0]
     y1_valid = data[:, 1]
+
+def poly_20(x, weight_list):
+    acc = weight_list[0]
+    for n in range(1, 21):
+        acc += (x**n)*weight_list[n]
+    return acc
