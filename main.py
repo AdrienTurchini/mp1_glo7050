cf1fe108feb6a8e8bbffd7e748dcf6bc01fdd39a
peopleOfPalay <marck.henriot@gmail.com>
2021-02-11 17:21:49 +0100

    open csv

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 import numpy as np
+import csv
+
+with open('Datasets\Dataset_1_test.csv') as csvfile:
+    data = np.array(list(csv.reader(csvfile)))
+    data = data[:, :2].astype(np.float)
+    X1_test = data[:, 0]
+    y1_test = data[:, 1]
+
+with open('Datasets\Dataset_1_train.csv') as csvfile:
+    data = np.array(list(csv.reader(csvfile)))
+    data = data[:, :2].astype(np.float)
+    X1_train = data[:, 0]
+    y1_train = data[:, 1]
+
+with open('Datasets\Dataset_1_valid.csv') as csvfile:
+    data = np.array(list(csv.reader(csvfile)))
+    data = data[:, :2].astype(np.float)
+    X1_valid = data[:, 0]
+    y1_valid = data[:, 1]
