1f754acb3af8ae67e42dc77c15f7d8dfc5dde7cf
peopleOfPalay <marck.henriot@gmail.com>
2021-02-16 19:27:40 +0100

    ridge_exo3

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd
 import csv
 import matplotlib.pyplot as plt
-import random 
+import random
 
 PLOT_1 = False
 PLOT_2 = False
@@ -56,7 +56,8 @@ with open('Datasets/Dataset_2_valid.csv') as csvfile:
     y2_valid = data[:, 1]
 
 names = pd.read_csv('Datasets/attributes.csv', delim_whitespace=True)
-data = pd.read_csv('Datasets/communities.data', names=names['names']).replace('?', np.NaN)
+data = pd.read_csv('Datasets/communities.data',
+                   names=names['names']).replace('?', np.NaN)
 
 print(data.head())
 data = data.loc[:, data.columns != 'communityname']
@@ -76,12 +77,15 @@ print(data.head())
 #########################################################################################################
 ############################################ FUNCTIONS EXO 1 ############################################
 #########################################################################################################
+
+
 def transform(X):
     vandermonde = np.ones((50, 1))
     for j in range(1, 21):
         x_pow = np.power(X, j)
         vandermonde = np.append(vandermonde, x_pow.reshape(-1, 1), axis=1)
     return vandermonde
+
 
 def fit(X, y, epoch=100):
     w = np.zeros(21)
@@ -91,6 +95,7 @@ def fit(X, y, epoch=100):
         w = w - (1/50)*(vandermonde.T @ (y_pred - y))
     return w
 
+
 def fit_L2(X, y, L2, epoch=50):
     w = np.zeros(21)
     vandermonde = transform(X)
@@ -99,8 +104,10 @@ def fit_L2(X, y, L2, epoch=50):
         w = w - (0.5*(vandermonde.T @ (y_pred - y)) + (L2 * w))/50
     return w
 
+
 def predict(X, w):
     return transform(X) @ w
+
 
 def MSE(y_pred, y):
     return np.square(y_pred - y).mean()
@@ -108,8 +115,10 @@ def MSE(y_pred, y):
 #########################################################################################################
 ############################################ FUNCTIONS EXO 2 ############################################
 #########################################################################################################
+
+
 class LinearReg:
-    def __init__(self, epochs=20, learning_rate=1e-6, X_valid = [], y_valid = []):
+    def __init__(self, epochs=20, learning_rate=1e-6, X_valid=[], y_valid=[]):
         #y = a*x + b
         self.a = 0
         self.b = 0
@@ -130,18 +139,18 @@ class LinearReg:
         for epoch in range(self.epochs):
             y_pred = self.predict(X)
 
-            error = y_pred - y 
+            error = y_pred - y
 
             grad_a = X*error/self.N
             grad_b = error/self.N
 
             self.a = self.a - self.lr * grad_a
             self.b = self.b - self.lr * grad_b
-            
+
             MSE = self.MSE(y_pred, y)
             MSE_tmp.append(MSE)
             #print(f'    Epoch: {epoch+1}/{self.epochs}, MSE={MSE}')
-        
+
         self.MSE_global.append(MSE_tmp)
 
     def SGD_plt(self, X, y):
@@ -149,19 +158,19 @@ class LinearReg:
         for epoch in range(self.epochs):
             y_pred = self.predict(X)
 
-            error = y_pred - y 
+            error = y_pred - y
 
             grad_a = X*error/self.N
             grad_b = error/self.N
 
             self.a = self.a - self.lr * grad_a
             self.b = self.b - self.lr * grad_b
-            
+
             MSE = self.MSE(y_pred, y)
             MSE_tmp.append(MSE)
 
         self.MSE_global.append(MSE_tmp)
-    
+
     def fit(self, X, y):
         self.N = len(X)
         for Xi, yi, i in zip(X, y, range(self.N)):
@@ -173,45 +182,58 @@ class LinearReg:
             self.SGD(Xi, yi)
             y_pred_valid = self.predict(self.X_valid)
             mse_valid = self.MSE(y_pred_valid, self.y_valid)
-            print(f"MSE pour le jeu de validation après {i+1} données d'entrainement, 50 epochs par donnée et un pas de {self.lr} = {mse_valid}")
+            print(
+                f"MSE pour le jeu de validation après {i+1} données d'entrainement, 50 epochs par donnée et un pas de {self.lr} = {mse_valid}")
 
     def fit_and_plot_valid(self, X, y):
         self.N = len(X)
         figure_exo2_2, axis_exo2_2 = plt.subplots(3, 2)
-        axis_exo2_2[0][0].scatter(self.X_valid, self.y_valid, label = "y_valid")
-        axis_exo2_2[0][1].scatter(self.X_valid, self.y_valid, label = "y_valid")
-        axis_exo2_2[1][0].scatter(self.X_valid, self.y_valid, label = "y_valid")
-        axis_exo2_2[1][1].scatter(self.X_valid, self.y_valid, label = "y_valid")
-        axis_exo2_2[2][0].scatter(self.X_valid, self.y_valid, label = "y_valid")
+        axis_exo2_2[0][0].scatter(self.X_valid, self.y_valid, label="y_valid")
+        axis_exo2_2[0][1].scatter(self.X_valid, self.y_valid, label="y_valid")
+        axis_exo2_2[1][0].scatter(self.X_valid, self.y_valid, label="y_valid")
+        axis_exo2_2[1][1].scatter(self.X_valid, self.y_valid, label="y_valid")
+        axis_exo2_2[2][0].scatter(self.X_valid, self.y_valid, label="y_valid")
         for Xi, yi, i in zip(X, y, range(self.N)):
             self.SGD_plt(Xi, yi)
             y_valid_pred = self.predict(self.X_valid)
-            
+
             if i == 0:
-                axis_exo2_2[0][0].scatter(self.X_valid, y_valid_pred, label = "y_pred")
+                axis_exo2_2[0][0].scatter(
+                    self.X_valid, y_valid_pred, label="y_pred")
                 axis_exo2_2[0][0].legend()
-                axis_exo2_2[0][0].set_title(f"y_pred après entrainement sur {i+1} données")
+                axis_exo2_2[0][0].set_title(
+                    f"y_pred après entrainement sur {i+1} données")
             elif i == 75:
-                axis_exo2_2[0][1].scatter(self.X_valid, y_valid_pred, label = "y_pred")
+                axis_exo2_2[0][1].scatter(
+                    self.X_valid, y_valid_pred, label="y_pred")
                 axis_exo2_2[0][1].legend()
-                axis_exo2_2[0][1].set_title(f"y_pred après entrainement sur {i+1} données")
+                axis_exo2_2[0][1].set_title(
+                    f"y_pred après entrainement sur {i+1} données")
             elif i == 150:
-                axis_exo2_2[1][0].scatter(self.X_valid, y_valid_pred, label = "y_pred")
+                axis_exo2_2[1][0].scatter(
+                    self.X_valid, y_valid_pred, label="y_pred")
                 axis_exo2_2[1][0].legend()
-                axis_exo2_2[1][0].set_title(f"y_pred après entrainement sur {i+1} données")
+                axis_exo2_2[1][0].set_title(
+                    f"y_pred après entrainement sur {i+1} données")
             elif i == 225:
-                axis_exo2_2[1][1].scatter(self.X_valid, y_valid_pred, label = "y_pred")
+                axis_exo2_2[1][1].scatter(
+                    self.X_valid, y_valid_pred, label="y_pred")
                 axis_exo2_2[1][1].legend()
-                axis_exo2_2[1][1].set_title(f"y_pred après entrainement sur {i+1} données")
+                axis_exo2_2[1][1].set_title(
+                    f"y_pred après entrainement sur {i+1} données")
             elif i == 299:
-                axis_exo2_2[2][0].scatter(self.X_valid, y_valid_pred, label = "y_pred")
+                axis_exo2_2[2][0].scatter(
+                    self.X_valid, y_valid_pred, label="y_pred")
                 axis_exo2_2[2][0].legend()
-                axis_exo2_2[2][0].set_title(f"y_pred après entrainement sur {i+1} données")
+                axis_exo2_2[2][0].set_title(
+                    f"y_pred après entrainement sur {i+1} données")
         plt.show()
 
 ###################################################################################################
 ############################################## CODE ###############################################
 ###################################################################################################
+
+
 def exo1():
     w = fit(X1_train, y1_train)
     y1_valid_pred = predict(X1_valid, w)
@@ -219,7 +241,6 @@ def exo1():
 
     MSE_valid = MSE(y1_valid_pred, y1_valid)
     MSE_train = MSE(y1_train_pred, y1_train)
-
 
     ############################################### L2 ################################################
     y1_valid_pred_L2s, y1_train_pred_L2s = [], []
@@ -247,17 +268,19 @@ def exo1():
     ###################################################################################################
     ############################################# GRAPHS ##############################################
     ###################################################################################################
-    figure, axis = plt.subplots(2, 1) 
+    figure, axis = plt.subplots(2, 1)
 
     axis[0].scatter(X1_train, y1_train, label='y_train')
     axis[0].scatter(X1_train, y1_train_pred, label='y_pred')
     axis[0].legend()
-    axis[0].set_title(f'Regression Polynomiale De Degrée 20 Sur Train\nMSE={MSE_train}')
+    axis[0].set_title(
+        f'Regression Polynomiale De Degrée 20 Sur Train\nMSE={MSE_train}')
 
     axis[1].scatter(X1_valid, y1_valid, label='y_valid')
     axis[1].scatter(X1_valid, y1_valid_pred, label='y_pred')
     axis[1].legend()
-    axis[1].set_title(f'Regression Polynomiale De Degrée 20 Sur Valid\nMSE={MSE_valid}')
+    axis[1].set_title(
+        f'Regression Polynomiale De Degrée 20 Sur Valid\nMSE={MSE_valid}')
     plt.show()
 
     ############################################### L2 ################################################
@@ -268,12 +291,14 @@ def exo1():
     plt.xlabel('$lambda$')
     plt.ylabel('MSE')
     plt.show()
-    
+
+
 def exo2():
     print("-----------------")
     print("QUESTION 1")
     print("-----------------")
-    model = LinearReg(epochs=50, learning_rate=1e-6, X_valid = X2_valid, y_valid = y2_valid)
+    model = LinearReg(epochs=50, learning_rate=1e-6,
+                      X_valid=X2_valid, y_valid=y2_valid)
     model.fit_and_MSE_valid(X2_train, y2_train)
     y2_train_pred = model.predict(X2_train)
     y2_train_MSE = MSE(y2_train_pred, y2_train)
@@ -282,53 +307,55 @@ def exo2():
     y2_valid_MSE = MSE(y2_valid_pred, y2_valid)
 
     print("\nOn remarque que le MSE diminue plus le nombre de données d'entrainement est grand, cependant le pas semble trop petit pour voir une nette diminution de la MSE")
-    
+
     # question 2
     print("-----------------")
     print("QUESTION 2")
     print("-----------------")
     pas = [i/100 for i in range(20, 200, 5)]
     _MSE = []
-    for i in pas :
-        model = LinearReg(learning_rate = i, X_valid = X2_valid, y_valid = y2_valid)
+    for i in pas:
+        model = LinearReg(learning_rate=i, X_valid=X2_valid, y_valid=y2_valid)
         model.fit(X2_train, y2_train)
 
         y2_valid_pred = model.predict(X2_valid)
         y2_valid_MSE = MSE(y2_valid_pred, y2_valid)
 
         _MSE.append(y2_valid_MSE)
-    
-    for i in range(len(pas)):
-        print(f"Jeu de validation, Epochs = 50, Pas = {pas[i]} --> MSE = {_MSE[i]}")
 
-    figure_exo2_1, axis_exo2_1 = plt.subplots(1, 1) 
+    for i in range(len(pas)):
+        print(
+            f"Jeu de validation, Epochs = 50, Pas = {pas[i]} --> MSE = {_MSE[i]}")
+
+    figure_exo2_1, axis_exo2_1 = plt.subplots(1, 1)
     axis_exo2_1.plot(pas, _MSE)
     axis_exo2_1.set_xlabel("pas")
     axis_exo2_1.set_ylabel("MSE")
     axis_exo2_1.set_title("MSE selon le pas utilisé et avec 50 epochs")
     plt.show()
-    
+
     print("\nLe pas qui nous donne la MSE la plus faible est 0.95, nous le garderons donc par la suite.")
     print("-----")
-    modelFinal = LinearReg(epochs = 1, learning_rate= 0.95, X_valid= X2_valid, y_valid= y2_valid)
+    modelFinal = LinearReg(epochs=1, learning_rate=0.95,
+                           X_valid=X2_valid, y_valid=y2_valid)
     modelFinal.fit(X2_train, y2_train)
 
     y2_valid_pred = modelFinal.predict(X2_valid)
     y2_valid_MSE = MSE(y2_valid_pred, y2_valid)
-    print(f"La MSE du jeu de test pour le modèle final avec 50 epochs et un pas de 0.9 est de : {y2_valid_MSE}")
-    
+    print(
+        f"La MSE du jeu de test pour le modèle final avec 50 epochs et un pas de 0.9 est de : {y2_valid_MSE}")
 
     # question 3
     print("-----------------")
     print("QUESTION 3")
     print("-----------------")
     modelFinal.fit_and_plot_valid(X2_train, y2_train)
-    
+
 
 #########################################################################################################
 ############################################ EXO 3 ######################################################
 #########################################################################################################
-def train_test_split_kfold(dataset, split = 0.8):
+def train_test_split_kfold(dataset, split=0.8):
     train = list()
     train_size = 0.8 * len(data)
     test = np.array(data)
@@ -366,7 +393,7 @@ def train_test_split_kfold(dataset, split = 0.8):
             train4.append(train[index])
         else:
             train5.append(train[index])
-        index+=1
+        index += 1
 
     index = 0
     while(index < test_size):
@@ -381,16 +408,46 @@ def train_test_split_kfold(dataset, split = 0.8):
             test4.append(test[index])
         else:
             test5.append(test[index])
-        index+=1
+        index += 1
 
     return train1, train2, train3, train4, train5, test1, test2, test3, test4, test5
 
 
+class RidgeRegression():
+    def __init__(self, learning_rate, epochs, l2):
+        self.lr = learning_rate
+        self.epochs = epochs
+        self.l2 = l2
+
+    def fit(self, X, y):
+        self.m, self.n = X.shape
+        self.w = np.zeros(self.n)
+        self.b = 0
+        self.X = X
+        self.y = y
+
+        for i in range(self.epochs):
+            self.ridge_gradiant()
+
+    def ridge_gradiant(self):
+        y_pred = self.predict(self.X)
+
+        error = y_pred - y
+
+        d_w = ((self.X.T @ error) + self.l2*self.W)/self.m
+        d_b = np.sum(error)/self.m
+
+        self.w = self.w - self.lr * d_w
+        self.b = self.b - self.lr * d_b
+
+    def predict(self, X):
+        return X @ self.w + self.b
+
 
 def exo3():
     print("On peut utiliser la moyenne de l'échantillon de chaque colonne. Cela nous permet de pouvoir travailler correctement avec les données. Cependant cela implique que nous sous-estimons la variance de nos données.")
-    
-    # Train Test Split + 5 fold 
+
+    # Train Test Split + 5 fold
     train1 = []
     train2 = []
     train3 = []
@@ -402,11 +459,11 @@ def exo3():
     test4 = []
     test5 = []
 
-    train1, train2, train3, train4, train5, test1, test2, test3, test4, test5 = train_test_split_kfold(data)
+    train1, train2, train3, train4, train5, test1, test2, test3, test4, test5 = train_test_split_kfold(
+        data)
 
 
 if __name__ == '__main__':
-    exo1()
-    exo2()
+    # exo1()
+    # exo2()
     exo3()
-    
